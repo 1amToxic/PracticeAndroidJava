3fbdea8da67ae15c6306243ca7902ef23e8938f9
Shiba <lamitptit99@gmail.com>
2021-05-28 01:10:58 +0700

    add require libs

---
+++ app/src/main/java/com/example/practiceandroidjava/App.java
@@ -0,0 +1,17 @@
+package com.example.practiceandroidjava;
+
+import android.app.Application;
+
+import androidx.viewbinding.BuildConfig;
+
+import timber.log.Timber;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+}
